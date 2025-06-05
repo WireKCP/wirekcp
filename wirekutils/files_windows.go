d37f131c4db0a95725593e9d6cce1c4d53eff022
Lim Shi Song <limshisong123@gmail.com>
2025-06-05 18:22:17 +0800

    Add support for Linux binary format in goreleaser and clean up Windows file installation logic

--- wirekutils/files_windows.go
+++ wirekutils/files_windows.go
@@ -2,24 +2,6 @@
 
 package wirekutils
 
-import (
-	"os"
-)
-
 func CheckOrInstallWinTun() error {
-	filename := "wintun.dll"
-	destination := "C:\\Windows\\System32\\wintun.dll"
-	// Check if the file exists, if not link it
-	if _, err := os.Stat(destination); os.IsNotExist(err) {
-		cwd, err := os.Getwd()
-		println("Current working directory: " + cwd)
-		if err != nil {
-			return err
-		}
-		file := cwd + "\\" + filename
-		println("Linking " + file + " to " + destination)
-		// Link the file to the destination
-		return os.Link(file, destination)
-	}
 	return nil
 }
